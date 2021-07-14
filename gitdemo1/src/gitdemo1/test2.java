e3c8e875fb282ca70d67241331d41ae090486ea6
Pref1x2k <aditya.yuvaraj.kshirsagar@gmail.com>
2021-07-14 15:21:44 +0530

    First commit

---
+++ gitdemo1/src/gitdemo1/test2.java
@@ -0,0 +1,14 @@
+package gitdemo1;
+
+public class test2 {
+	 public static void main (String[] args) {
+		 String hungry  = "yes";
+		 
+		 if (hungry == "yes")
+			 System.out.println("Eat");
+		 else {
+			 System.out.println("Work");
+		 }
+	 }
+
+}
